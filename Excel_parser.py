be53a1c78c6dc59bc7481256dcdaa6a776327799
Bob Terminator <igor_zhurbenko@mail.ru>
2020-08-12 01:24:34 +0700

    Clean

--- Excel_parser.py
+++ Excel_parser.py
@@ -1,6 +1,11 @@
 import openpyxl
 
 
+def log(message):
+    f = open('out.txt', 'w')
+    f.write(str(message))
+    f.close()
+
 def empty(string : str):
     return string == None or string.strip() == ''
     
@@ -37,16 +42,31 @@ while (str(doc['A' + str(currentline)].value).count('/') < 5):
         obj1['problems'] = []
         obj1['tools'] = []
         obj1['methods'] = []
-        obj1['indicators'] = []
+        #obj1['indicators'] = []
         #currentline+=1
+
+        problem = {}
+        problem['problem'] = line()['d']
+        problem['indicators'] = []
+
         while (empty(doc['A' + str(currentline)].value) or doc['b' + str(currentline)].value == obj1['level']):
             cl = line()
-            addIfNotEmpty(obj1['problems'],cl['d'])
+            problemName = cl['d'] if bool(cl['d']) else problemName
+            if problem['problem'] == problemName:
+                addIfNotEmpty(problem['indicators'],cl['j'])
+            else:
+                obj1['problems'].append(problem)
+                problem = {}
+                problem['problem'] = problemName
+                problem['indicators'] = []
+                addIfNotEmpty(problem['indicators'],cl['j'])
+
             addIfNotEmpty(obj1['tools'],cl['h'])
             addIfNotEmpty(obj1['methods'],cl['f'])
-            addIfNotEmpty(obj1['indicators'],cl['j'])
+            #addIfNotEmpty(obj1['indicators'],cl['j'])
             currentline+=1
             cl = line()
+        obj1['problems'].append(problem)
         obj0['levels'].append(obj1)
         #currentline += 1
         if cl['a'].count('/') > 3:
@@ -57,14 +77,15 @@ while (str(doc['A' + str(currentline)].value).count('/') < 5):
         break;
     currentline += 1
     
+#print(res[0])
 
 a = open('generatedfromexcel.js', 'w')
 
 filecontent = 'SubjectsList = []; \n SubjectsInfoJson = {}; \n'
 
 for subj in res:
-    filecontent += 'SubjectsInfoJson[' + '"' + subj['subject'].replace('-','__') + '".replace(" ","_")' + '] = ' + str(subj['levels']).replace('-','__') + '\n'
-    filecontent += 'SubjectsList.push(' + '"' + subj['subject'].replace('-','__') + '".replace(" ","_")' + '); \n\n\n'
+    filecontent += 'SubjectsInfoJson[' + '"' + subj['subject'].replace('-','__') + '".split(" ").join("_")' + '] = ' + str(subj['levels']).replace('-','__') + '\n'
+    filecontent += 'SubjectsList.push(' + '"' + subj['subject'].replace('-','__') + '".split(" ").join("_")' + '); \n\n\n'
 
 a.write(filecontent)
 a.close()
--- Script.js
+++ Script.js
@@ -5,7 +5,31 @@ function normalizeString(str) {
     return (str[0].toUpperCase() + str.slice(1)).split('_').join(' ');
 }
 
+function uploadFields(list, field)
+{
+    res = [];
+    for (var line of list)
+    {
+        res.push(line[field])
+    }
+    return res;
+}
 
+function determineIndicatorsVisibility(optionsLine)
+{
+    var i = 0;
+    while (optionsLine['_' + String(i)] != undefined)
+    {
+        if (!optionsLine['_' + String(i)].Value)
+        {
+            indicatorsList.hidden = true;
+            return false;
+        }
+        i++;
+    }
+    indicatorsList.hidden = false;
+    return true;
+}
 
 class Options {
     constructor(optionsGiven, appendTo, onselect, name) {
@@ -38,7 +62,7 @@ class Options {
         }
 
         var label = document.createElement('p');
-        label.innerHTML = name + ': ';
+        label.innerHTML = name + ' ';
         this.label = label;
 
         newColumn.appendChild(label);
@@ -80,29 +104,74 @@ class OptionsLine {
         
         for (var num in OptionsList) {
             var i = Number(num);
-            this['_' + String(i)] = new Options(OptionsList[i], tbody,
-                function () {
-                    var j = 1;
-                    while (self['_' + String(Number(this.owner.number) + j)] != undefined) {
-                        var isNotEmpty = Boolean(self['_' + String(Number(this.owner.number) + j - 1)].Value);
-                        var isNotHidden = !Boolean(self['_' + String(Number(this.owner.number) + j - 1)].selector.hidden);
-                        self['_' + String(Number(this.owner.number) + j)].switch(
-                            isNotEmpty
-                            && isNotHidden &&
-                            self['_' + String(Number(this.owner.number + 1) + (j-1))] != undefined)
-                        j++;
-                    }
-                    j = 0;
-                    self.signature.innerHTML = "";
-                    while (self['_' + Number(j)] != undefined && self['_' + j].Value)
-                    {
-                        var bindingWord = Boolean(bindingWords[j]) ? bindingWords[j] : "";
-                        self.signature.innerHTML = self.signature.innerHTML + " " + self['_' + j].Value + " " + bindingWord;
-                        
-                        j++;
-                    }
 
-                }, optionsNames[i]);
+            if (typeof (OptionsList[num][0]) == typeof ('')) {
+                this['_' + String(i)] = new Options(OptionsList[i], tbody,
+                    function () {
+                        var j = 1;
+                        while (self['_' + String(Number(this.owner.number) + j)] != undefined) {
+                            var isNotEmpty = Boolean(self['_' + String(Number(this.owner.number) + j - 1)].Value);
+                            var isNotHidden = !Boolean(self['_' + String(Number(this.owner.number) + j - 1)].selector.hidden);
+                            self['_' + String(Number(this.owner.number) + j)].switch(
+                                isNotEmpty
+                                && isNotHidden &&
+                                self['_' + String(Number(this.owner.number + 1) + (j - 1))] != undefined)
+                            j++;
+                        }
+                        j = 0;
+                        self.signature.innerHTML = "";
+                        while (self['_' + Number(j)] != undefined && self['_' + j].Value) {
+                            var bindingWord = Boolean(bindingWords[j]) ? bindingWords[j] : "";
+                            self.signature.innerHTML = self.signature.innerHTML + " " + self['_' + j].Value + " " + bindingWord;
+
+                            j++;
+                        }
+                        determineIndicatorsVisibility(self);
+
+                    }, optionsNames[i]);
+            }
+            else
+            {
+                var problemsNames = uploadFields(OptionsList[i], 'problem');
+                    this['_' + String(i)] = new Options(problemsNames, tbody,
+                    function () {
+                        var j = 1;
+                        while (self['_' + String(Number(this.owner.number) + j)] != undefined) {
+                            var isNotEmpty = Boolean(self['_' + String(Number(this.owner.number) + j - 1)].Value);
+                            var isNotHidden = !Boolean(self['_' + String(Number(this.owner.number) + j - 1)].selector.hidden);
+                            self['_' + String(Number(this.owner.number) + j)].switch(
+                                isNotEmpty
+                                && isNotHidden &&
+                                self['_' + String(Number(this.owner.number + 1) + (j - 1))] != undefined)
+                            j++;
+                        }
+                        j = 0;
+                        self.signature.innerHTML = "";
+                        while (self['_' + Number(j)] != undefined && self['_' + j].Value) {
+                            var bindingWord = Boolean(bindingWords[j]) ? bindingWords[j] : "";
+                            self.signature.innerHTML = self.signature.innerHTML + " " + self['_' + j].Value + " " + bindingWord;
+
+                            j++;
+                        }
+
+                        this.problemsInfo = OptionsList[0];
+                        window.indicators = this.problemsInfo[this.selectedIndex]['indicators'];
+
+                        while (window.indicatorsList.firstChild) {
+                            window.indicatorsList.removeChild(window.indicatorsList.firstChild);
+                        }
+
+                        for (var indicator of window.indicators)
+                        {
+                            var newLine = document.createElement('li');
+                            newLine.innerHTML = indicator;
+                            window.indicatorsList.appendChild(newLine);
+                        }
+
+                        determineIndicatorsVisibility(self);
+
+                    }, optionsNames[i]);
+            }
             this["_" + i].selector.owner = this["_" + i];
             this["_" + i].switch(num < 1);
             this["_" + i].number = i;
@@ -114,7 +183,8 @@ class OptionsLine {
         this.signature.style.textAlign = 'center';
         appendTo.appendChild(this.signature);
 
-
+        window.indicatorsList = document.createElement('ul');
+        appendTo.appendChild(window.indicatorsList);
     }
 
     GetValue(index) {
